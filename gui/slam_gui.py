dfb16728b45479f4f1b08d18e580f0feb21e9653
goldensquirrel <goldensquirrel11@gmail.com>
2026-06-22 09:21:27 +0800

    Rename frontend FPS variables

--- gui/slam_gui.py
+++ gui/slam_gui.py
@@ -51,8 +51,8 @@ class SLAM_GUI:
         self.background = None
 
         self.last_memory_update = 0
-        self.fps_start_time = time.time()
-        self.frame_count = 0
+        self.frontend_fps_start = time.time()
+        self.frontend_frame_count = 0
 
         try:
             pynvml.nvmlInit()
@@ -465,15 +465,15 @@ class SLAM_GUI:
             )
 
             # Update frontend FPS
-            self.frame_count += 1
+            self.frontend_frame_count += 1
             current_time = time.time()
-            elapsed_time = current_time - self.fps_start_time
+            elapsed_time = current_time - self.frontend_fps_start
 
             if elapsed_time >= 1:
-                fps = self.frame_count / elapsed_time
+                fps = self.frontend_frame_count / elapsed_time
                 self.frontend_fps.text = f"Tracking FPS: {fps:.1f}"
-                self.frame_count = 0
-                self.fps_start_time = current_time
+                self.frontend_frame_count = 0
+                self.frontend_fps_start = current_time
 
             if self.followcam_chbox.checked:
                 viewpoint = (
